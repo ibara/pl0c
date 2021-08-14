df86d930de9955b8946d6df9a70ae47b4336f156
Brian Callahan <bcallah@openbsd.org>
2021-08-14 12:26:08 -0400

    Fix grammar comment.

--- pl0c.c
+++ pl0c.c
@@ -63,10 +63,10 @@
  *		  [ "var" ident { "," ident } ";" ]
  *		  { "procedure" ident ";" block ";" } statement .
  * statement	= [ ident ":=" expression
- *		| "call" ident
- *		| "begin" statement { ";" statement } "end"
- *		| "if" condition "then" statement
- *		| "while" condition "do" statement ] .
+ *		  | "call" ident
+ *		  | "begin" statement { ";" statement } "end"
+ *		  | "if" condition "then" statement
+ *		  | "while" condition "do" statement ] .
  * condition	= "odd" expression
  *		| expression ( "=" | "#" | "<" | ">" ) expression .
  * expression	= [ "+" | "-" ] term { ( "+" | "-" ) term } .
